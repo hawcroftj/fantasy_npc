2795bbe72d60ef09e0e6e8dcb3767826a562a216
Jadon H <hawcroftj@gmail.com>
2018-05-11 19:54:41 -0500

    Modified application structure - data will now be read from .json asset files. SQLite will be used only to store created characters. Added asset folder to gitignore.

--- app/src/main/java/hawcroftj/github/com/fantasynpc/DatabaseHelper.java
+++ app/src/main/java/hawcroftj/github/com/fantasynpc/DatabaseHelper.java
@@ -17,14 +17,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String CHARACTER_COL_3 = "LASTNAME";
     public static final String CHARACTER_COL_4 = "RACE";
     public static final String CHARACTER_COL_5 = "CLASS";
-//    // First Name table
-//    public static final String FNAME_TABLE_NAME = "fname_table";
-//    // Last Name table
-//    public static final String LNAME_TABLE_NAME = "lname_table";
-//    // Race table
-//    public static final String RACE_TABLE_NAME = "race_table";
-//    // Class table
-//    public static final String CLASS_TABLE_NAME = "class_table";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
--- app/src/main/java/hawcroftj/github/com/fantasynpc/MainActivity.java
+++ app/src/main/java/hawcroftj/github/com/fantasynpc/MainActivity.java
@@ -1,13 +1,45 @@
 package hawcroftj.github.com.fantasynpc;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    //DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // create new database
+        //db = new DatabaseHelper(this);
+        TextView tvJson = findViewById(R.id.tvJson);
+        String json = loadJSONFromAsset(getApplicationContext());
+        tvJson.setText(json);
+    }
+
+    public String loadJSONFromAsset(Context context) {
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open("races/human.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
     }
 }
